7bf818dd3167e6443b95ed872bf3a2dafd5dd9e1
Gustavo Ramírez Hidalgo <tavroot@gmail.com>
2014-02-16 01:00:15 -0600

    intercambio entre proyecto mod 5 y proyecto final...

---
+++ ProyectoFinal/programa/transformer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Resolucion recomendada: 1280x710, para Acer Aspire 14 pulgadas
+resolucion=1280x710
+
+#Specify ffmpeg PATH
+ffmpegPATH=/home/gustavo/Downloads/src0/ffmpeg-0.11.1/ffmpeg
+
+directVideos=../videos/
+transformedPath=../videos/transf/
+rm -R $transformedPath
+mkdir $transformedPath
+
+#echo "cosa"
+stringList=`ls $directVideos`
+arrayList=(${stringList//\ / })
+
+#echo ${#arrayList[@]}
+
+counter=0
+for i in $(ls $directVideos)
+do
+    fileToProcess=${arrayList[$counter]}
+    #echo $fileToProcess
+    if [[ -d $directVideos$fileToProcess ]]; then
+        continue
+        #echo "Procesando archivo "${arrayList[$counter]}"..."
+    else
+        echo ""
+        echo "Procesando archivo "${arrayList[$counter]}"..."
+        $ffmpegPATH -i $directVideos$fileToProcess -s $resolucion -b:v 512k -vcodec mpeg1video -acodec copy "TRANSFORMED"$fileToProcess
+        mv ./TRANSFORMED$fileToProcess $transformedPath
+        #transformedPath
+    fi
+    counter=`expr $counter + 1`
+done
+
+echo ""
+echo "Transformaciones finalizadas..."
+
+#echo ${arrayList[0]}
