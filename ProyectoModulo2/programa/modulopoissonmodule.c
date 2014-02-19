4200404485e81b7df5badaef26a47e7a07d23b4d
Gustavo Ramírez Hidalgo <tavroot@gmail.com>
2014-02-19 16:07:28 -0600

    trabajos sobre modulo de C y documentacion, modulo 2

--- ProyectoModulo2/programa/modulopoissonmodule.c
+++ ProyectoModulo2/programa/modulopoissonmodule.c
@@ -1,15 +1,42 @@
 #include <Python.h>
 //#include <string.h>
+#include <math.h>
 
 /*
  * Confg de mod en C para la resolucion de la ecuacion de Poisson
  * 
  */
 
+
+//Funciones matematicas...
+//Funcion densidad, ya fue probada y funciona, recibe valores en radianes
+double FnDensidad(double x, double y){
+    return -(cos(x+y)+cos(x-y));
+}
+
+//Condiciones de frontera
+//Ya fueron probadas y funcionan, reciben valores en radianes
+double FnFronteraIzquierda(double y){
+    return cos(y);
+}
+double FnFronteraDerecha(double y){
+    return -cos(y);
+}
+
+double FnFronteraInferior(double x){
+    return cos(x);
+}
+double FnFronteraSuperior(double x){
+    return 0;
+}
+
+
+
 // Función modulopoisson.solvepoisson()
 static PyObject *modulopoisson_solvepoisson(PyObject *self, PyObject *args) {
 
-    int i;
+    //Contadores...
+    int i,j;
     PyObject *lista;
     if(!PyArg_ParseTuple(args, "O", &lista))
         return NULL;
@@ -31,12 +58,77 @@ static PyObject *modulopoisson_solvepoisson(PyObject *self, PyObject *args) {
     //Longitud del array de resultados...
     int lengthArrayResults;
     //code...
+    //double fnBLA=FnDensidad(4,5);
+    //printf("Densidad: %f", fnBLA);
+
+    //Primero, se crea el arreglo de resultados
+    double *arrayResults;
+    //Se define variable con total de datos en la grilla
+    double totalDatos=(datosingresados[4]+1)*(datosingresados[5]+1);
+    //Se define variable con pasos... h es para el eje x, k es para el y
+    double h=(datosingresados[1]-datosingresados[0])/datosingresados[5];
+    double k=(datosingresados[3]-datosingresados[2])/datosingresados[4];
+    arrayResults=(double *)malloc(totalDatos*sizeof(double));
+    //Se ingresan los valores de frontera izquierda
+    int counterToFill=0;
+    for(i=0; i<(int)totalDatos; i+=(int)(datosingresados[5]+1)){
+        arrayResults[i]=FnFronteraIzquierda(datosingresados[2]+counterToFill*k);
+        counterToFill++;
+    }
+    //Se ingresan los valores de frontera derecha
+    counterToFill=0;
+    for(i=datosingresados[5]; i<(int)totalDatos; i+=(int)(datosingresados[5]+1)){
+        arrayResults[i]=FnFronteraDerecha(datosingresados[2]+counterToFill*k);
+        counterToFill++;
+    }
+    //Se ingresan los valores de frontera inferior
+    counterToFill=0;
+    for(i=1; i<(int)datosingresados[5]; i++){
+        arrayResults[i]=FnFronteraInferior(datosingresados[0]+counterToFill*h);
+        counterToFill++;
+    }
+    //Se ingresan los valores de frontera superior
+    counterToFill=0;
+    for(i=(int)(totalDatos-1-datosingresados[5]+1); i<(int)(totalDatos-1); i++){
+        arrayResults[i]=FnFronteraSuperior(datosingresados[0]+counterToFill*h);
+        counterToFill++;
+    }
+    //Ahora se comienza el algoritmo...!
+    //1. el primer paso es la creacion de los pasos, lo cual ya esta hecho
+    //2. el segundo paso es la creacion de los valores espaciales de los puntos
+    //de grilla internos, lo cual ya esta hecho tambien.. hay que tener claro
+    //que las iteraciones se dan con i desde 1 hasta n-1, y con j desde 1 hasta
+    //m-1
+    //3. el tercer paso es el llenado de la grilla con ceros, lo cual esta hecho
+    //de manera automatica al solicitar memoria dinamicamente
+    //4. Se crean mas constantes, llamadas lambda, muC, eleC
+    double lambdaC=(h*h)/(k*k);
+    double muC=2*(1+lambdaC);
+    double eleC=1;
+    //5. se lleva a cabo un while, que controla el resto del flujo del programa,
+    //y se sale de este while en caso de superar el numero de iteraciones N... lo
+    //que se hace en este while son iteraciones de Gauss-Seidell
+    //Se declaran algunas variables antes del while, que se utilizaran ahi
+    double z, norm;
+/*
+    while(eleC<datosingresados[7]){
+        z=(-(h*h)*FnDensidad(datosingresados[0]+1*h,datosingresados[2]+(datosingresados[4]-1)*k)+FnFronteraIzquierda(datosingresados[2]+(datosingresados[4]-1)*k)+lambdaC*FnFronteraSuperior(datosingresados[0]+1*h)+lambdaC*arrayResults[(int)(1+(datosingresados[4]-2)*datosingresados[5])]+arrayResults[(int)(2+(datosingresados[4]-1)*datosingresados[5])])/muC;
+        norm=abs(z-arrayResults[(int)(1+(datosingresados[4]-1)*datosingresados[5])]);
+        arrayResults[(int)(1+(datosingresados[4]-1)*datosingresados[5])]=z;
+        for(i=2; i<(datosingresados[5])){
+            
+        }
+
+        eleC++;
+    }
+*/    
+
     //-----------------------------------------------------------------
 
 
 
     // Retorna un objeto Python, a partir del array resultante...
-    lengthArrayResults=8;
+    lengthArrayResults=(datosingresados[4]+1)*(datosingresados[5]+1);
 /*
     int lengthStrToPassPyObj=lengthArrayResults+2+(lengthArrayResults-1)+1; //ese 1 extra es para el caracter de escape \n
     char *strToPassPyObj;
@@ -55,7 +147,7 @@ static PyObject *modulopoisson_solvepoisson(PyObject *self, PyObject *args) {
 */
     PyObject* lista2 = PyList_New(0);
     for(i=0; i<lengthArrayResults; i++){
-        PyList_Append(lista2, Py_BuildValue("f", datosingresados[i]));
+        PyList_Append(lista2, Py_BuildValue("f", arrayResults[i]));
     }
     //return Py_BuildValue(strToPassPyObj, &datosingresados);
     //return Py_BuildValue("i", 56);
--- ProyectoModulo2/programa/programa.py
+++ ProyectoModulo2/programa/programa.py
@@ -37,32 +37,32 @@ print "\nPrograma para la visualizacion de la solucion a la ecuacion de Poisson:
 print "(todas las distancias estan en cm)"
 print "\n"
 endpointA=raw_input('Ingrese limite de la izquierda (a default=0): ')
-endpointB=raw_input('Ingrese limite de la derecha (b default=3): ')
+endpointB=raw_input('Ingrese limite de la derecha (b default=9): ')
 endpointC=raw_input('Ingrese limite de arriba (c default=0): ')
-endpointD=raw_input('Ingrese limite de abajo: (d default=1.5): ')
-stepM=raw_input('Ingrese cantidad de divisiones para el eje y (m default=5): ')
-stepN=raw_input('Ingrese cantidad de divisiones para el eje x (n default=5): ')
+endpointD=raw_input('Ingrese limite de abajo: (d default=4.5): ')
+stepM=raw_input('Ingrese cantidad de divisiones para el eje y (m default=15): ')
+stepN=raw_input('Ingrese cantidad de divisiones para el eje x (n default=15): ')
 tolerance=raw_input('Ingrese la tolerancia (tol default=0.1): ')
-maxIterationsN=raw_input('Ingrese el numero maximo de iteraciones (N default=20): ')
+maxIterationsN=raw_input('Ingrese el numero maximo de iteraciones (N default=50): ')
 
 #En caso de no ingresar objetos numericos, se hace un cambio del valor al default,
 #esto para cada uno de los ingresados
 if not(endpointA.isdigit()):
     endpointA=0
 if not(endpointB.isdigit()):
-    endpointB=3
+    endpointB=9
 if not(endpointC.isdigit()):
     endpointC=0
 if not(endpointD.isdigit()):
-    endpointD=1.5
+    endpointD=4.5
 if not(stepM.isdigit()):
-    stepM=5
+    stepM=15
 if not(stepN.isdigit()):
-    stepN=5
+    stepN=15
 if not(tolerance.isdigit()):
     tolerance=0.1
 if not(maxIterationsN.isdigit()):
-    maxIterationsN=20
+    maxIterationsN=50
 
 #Seria bueno agregar un verificado de la conmensurabilidad entre
 #los limites superiores y la cantidad de divisiones de los ejes,
@@ -73,6 +73,7 @@ if not(maxIterationsN.isdigit()):
 arrayRESULT=modulopoisson.solvepoisson([float(endpointA), float(endpointB), float(endpointC), float(endpointD), float(stepM), float(stepN), float(tolerance), float(maxIterationsN)])
 print "\nEjecutando resolucion a traves de modulo de C...\n"
 print arrayRESULT
+print len(arrayRESULT)
 
 
 #Codigo pesado aqui... VPython
