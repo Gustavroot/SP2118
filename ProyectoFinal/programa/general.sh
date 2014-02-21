55cc54103491e7acd0de2b2db66a9bdff4985fe2
Gustavo Ramírez Hidalgo <tavroot@gmail.com>
2014-02-21 02:40:16 -0600

    proyecto final: mas trabajos sobre general.sh, automatizando mas el proceso, dandole ademas la opcion al usuario de guardar o no los resultados obtenidos

--- ProyectoFinal/programa/general.sh
+++ ProyectoFinal/programa/general.sh
@@ -14,9 +14,18 @@ mkdir "RESULTS_$now/Intensidades"
 
 mv 2D_* "RESULTS_$now/TwoDim"
 
-currentDir=`pwd`
-#cd /home/$userD/LEEDcicima/
-#git pull origin master
+read -p "¿Desea almacenar la informacion recien procesada? (s o n)" answr
+if [ "$answr" == "s" ]
+then
+    currentDir=`pwd`
+    cd /home/$userD/LEEDcicima/
+    git pull origin master
+    cp $currentDir"/RESULTS_$now" -R .
+    git add -A
+    git commit -m "Analisis computacional de LEED: "$now
+    echo "Si se le pide un usuario y la correspondiente contraseña a continuacion, es la informacion de su usuario de Github:"
+    git push origin master
+fi
 
 
 #mv 2D_* ../RESULTS/
